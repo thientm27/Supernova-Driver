5ecd2039fdfe51166b567bbab9eff0675468c38c
Peace <khann@onechain.game>
2024-08-27 08:22:02 +0700

    mainscene and services

---
+++ Assets/SupernovaDriver/Scripts/Services/GameOver.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Elympics;
+using UnityEngine;
+
+public class GameOver : ElympicsMonoBehaviour, IInitializable
+{
+    public void Initialize()
+    {
+
+    }
+    public void EndGame(float score)
+    {
+        if (Elympics.IsServer)
+        {
+            Elympics.EndGame(new ResultMatchPlayerDatas(new List<ResultMatchPlayerData> { new ResultMatchPlayerData { MatchmakerData = new float[1] { score } } }));
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            EndGame(30);
+        }
+    }
+}
