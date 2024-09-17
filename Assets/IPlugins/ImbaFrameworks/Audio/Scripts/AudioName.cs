84c21a96fe7f522c395ae5e0a9bb44eaeba667d6
Peace <khann@onechain.game>
2024-09-17 10:05:25 +0700

    Iplugin

---
+++ Assets/IPlugins/ImbaFrameworks/Audio/Scripts/AudioName.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Imba.Audio
+{
+    [SerializeField]
+    public enum AudioName
+    {
+        // Main Music
+        Track_1 = -2,
+
+        NoSound = -1,
+
+        BGM_Menu     = 0,
+        BGM_GAMEPLAY = 1,
+        AMB_GAMEPLAY = 2,
+
+        #region UI
+
+        #endregion
+
+        #region GAME PLAY
+
+        Claim     = 30,
+        Claim2X   = 32,
+        CountDown = 33,
+        StartGame = 34,
+
+        Explotion      = 35,
+        Explotion2     = 36,
+        Explotion3     = 37,
+        SoundCarStart1 = 38,
+        SoundCarStart2 = 39,
+
+        #endregion
+    }
+
+    public enum AudioType
+    {
+        SFX = 0,
+        BGM = 1,
+        AMB = 2,
+    }
+}
---
+++ Assets/IPlugins/ImbaFrameworks/UI/Scripts/UIPopup/UIPopupName.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2015 - 2019 Imba
+// Author: Kaka
+// Created: 2019/08
+
+using UnityEngine;
+using System.Collections;
+
+namespace Imba.UI
+{
+    /// <summary>
+    /// Defines all of popup in game
+    /// </summary>
+    public enum UIPopupName
+    {
+        SamplePopup = -1,
+        MessageBox = 0,
+        MessageBoxUnclosePopup = 1,
+        SettingPopup = 2,
+        LevelUpPopup = 3,
+        SummaryPopup = 4,
+        UpgradeSkillPopup = 5,
+        EndGamePopup,
+    }
+}
